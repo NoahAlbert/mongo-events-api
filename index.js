1f1fdeff6b1a2ab72716086f1c1a243437f301a1
NoahAlbert <no.albert113@gmail.com>
2021-07-15 10:45:34 -0400

    established connection to mongo

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
-const dotenv = require('dotenv')
+require('dotenv/config')
 
 const app = express()
 app.use(express.json())
 
+mongoose
+    .connect(process.env.DB_CONNECTION, {useNewUrlParser: true , useUnifiedTopology: true})
+    .then(console.log('connected to mongo'))
+    .catch(err => console.log(err))
+
+app.get('/events', (req, res) => {
+
+})
 app.get('/', (req,res) => res.send('🏠 HOMEPAGE 📄'))
 
 
