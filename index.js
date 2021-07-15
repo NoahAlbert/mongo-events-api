f4c2590c312d496243f4ab2f7dfceba2c0f4d0d6
NoahAlbert <no.albert113@gmail.com>
2021-07-15 11:41:57 -0400

    fixed bug - moved all into one file

--- index.js
+++ index.js
@@ -3,13 +3,44 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 require('dotenv/config')
 
-const { createEvent } = import('./src/events.js')
+// const { createEvent } = import('./src/events.js')
 
 const app = express()
 app.use(express.json())
 
-app.get('/events', createEvent)
+mongoose
+    .connect(process.env.DB_CONNECTION, {useNewUrlParser: true , useUnifiedTopology: true})
+    .then(console.log('connected to mongo'))
+    .catch(err => console.log(err))
 
+const eventSchema = mongoose.Schema({
+    title: String,
+    date: Date,
+    description: String,
+    fee: String,
+    attendees: Array
+})
+
+const Event = mongoose.model('Event', eventSchema)
+
+const newEvent = {
+    title: "Boca Beach Cleanup",
+    date: "2021-08-02",
+    description: "Cleanup the beaches of Boca Raton",
+    fee: "Free",
+    attendees: ["Christian", "Mia", "Noah", "Emily", "Zack"]
+}
+
+function createEvent(req, res){
+    new Event(newEvent)
+    .save()
+    .then(console.log('Event Saved Successfully'))
+    .catch(err => console.log(err))
+}
+app.get('/events', (req, res) =>{
+    createEvent()
+    res.send('created successfully')
+})
 app.get('/', (req,res) => res.send('🏠 HOMEPAGE 📄'))
 
 
